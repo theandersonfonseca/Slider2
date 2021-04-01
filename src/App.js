c72c9253e867cb63ec97b87adf5c4a80dea14f84
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-04-01 18:01:58 -0300

    add scripts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,113 @@
+import React from 'react';
 import './App.css';
+import slides from './Slides';
 
-function App() {}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentSlide: slides[0],
+      previousSlide: slides[slides.length - 1],
+      nextSlide: slides[1],
+    };
+  }
+
+  getSlide = (currentSlide, nextOrPrevious) => {
+    let index = slides.findIndex((slide) => slide === currentSlide);
+    index = nextOrPrevious === 'next' ? index + 1 : index - 1;
+
+    if (!slides[index]) {
+      return nextOrPrevious === 'next' ? slides[0] : slides[slides.length - 1];
+    }
+
+    return slides[index];
+  };
+
+  updateState = (currentSlide) => {
+    this.setState({
+      currentSlide: currentSlide,
+      nextSlide: this.getSlide(currentSlide, 'next'),
+      previousSlide: this.getSlide(currentSlide, 'previous'),
+    });
+  };
+
+  componentDidMount = () => {
+    this.interval = setInterval(
+      () => this.updateState(this.state.nextSlide),
+      5000
+    );
+  };
+
+  componentWillUnmount = () => clearInterval(this.interval);
+
+  setPreviousSlide = () => {
+    this.componentWillUnmount();
+    this.updateState(this.state.previousSlide);
+    this.componentDidMount();
+  };
+
+  setNextSlide = () => {
+    this.componentWillUnmount();
+    this.updateState(this.state.nextSlide);
+    this.componentDidMount();
+  };
+
+  handleClickOnControl = (e) => {
+    const slide = e.target.alt;
+
+    if (!slide) return;
+
+    this.componentWillUnmount();
+    this.updateState(slide);
+    this.componentDidMount();
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <ul className="controls">
+          {slides.map((slide) => (
+            <li key={slide} onClick={this.handleClickOnControl}>
+              <img
+                className={
+                  this.state.currentSlide === slide
+                    ? 'control control-active'
+                    : 'control'
+                }
+                src={slide}
+                alt={slide}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <div className="slider">
+          <span className="previous" onClick={this.setPreviousSlide}>
+            {'<'}
+          </span>
+
+          <ul className="slides">
+            {slides.map((slide) => (
+              <li className="slide" key={slide}>
+                <img
+                  className={
+                    this.state.currentSlide === slide ? 'img img-active' : 'img'
+                  }
+                  src={slide}
+                  alt={slide}
+                />
+              </li>
+            ))}
+          </ul>
+
+          <span className="next" onClick={this.setNextSlide}>
+            {'>'}
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Slides.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Slide1 from './assets/foto1.jpg';
+import Slide2 from './assets/foto2.jpg';
+import Slide3 from './assets/foto3.jpg';
+import Slide4 from './assets/foto4.jpg';
+import Slide5 from './assets/foto5.jpg';
+import Slide6 from './assets/foto6.jpg';
+
+const slides = [Slide1, Slide2, Slide3, Slide4, Slide5, Slide6];
+
+export default slides;
